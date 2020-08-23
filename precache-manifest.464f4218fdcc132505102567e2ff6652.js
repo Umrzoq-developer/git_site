ebe2f4c55597fda1bfdb36cc7c2d675849496191
Umrzoq Toshkentov <utoshkentov@gmail.com>
2020-08-23 19:07:42 +0500

    Updates

---
+++ precache-manifest.464f4218fdcc132505102567e2ff6652.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb4de2ac99128790371be397a6575258",
+    "url": "/git_site/index.html"
+  },
+  {
+    "revision": "aa8f844e8d69b3e2a173",
+    "url": "/git_site/static/css/2.97ca6098.chunk.css"
+  },
+  {
+    "revision": "429b4c9e32be38b525a6",
+    "url": "/git_site/static/css/main.1a319894.chunk.css"
+  },
+  {
+    "revision": "aa8f844e8d69b3e2a173",
+    "url": "/git_site/static/js/2.d08245c8.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/git_site/static/js/2.d08245c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "429b4c9e32be38b525a6",
+    "url": "/git_site/static/js/main.88de19cd.chunk.js"
+  },
+  {
+    "revision": "47c212279655c17a795a",
+    "url": "/git_site/static/js/runtime-main.1d19ad91.js"
+  },
+  {
+    "revision": "8517a93bd3d53ef683619fa9af858ff9",
+    "url": "/git_site/static/media/logout.8517a93b.jpg"
+  }
+]);
